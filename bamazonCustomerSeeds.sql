d07dcd4fe1cfbbad76b045d497f48f028136e177
gerarjon <gerarsuaverdez@gmail.com>
2019-10-23 18:37:55 -0700

    added sample data

---
+++ bamazonCustomerSeeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO products (product_name, price, department_name, stock_quantity)
+VALUES 
+    ("socks", 2.50, "clothing", 2),
+    ("bread", 20.00, "food", 100),
+    ("peanut butter", 25.00, "food", 5),
+    ("Unlucky Charms", 3.95, "food", 60),
+    ("60 inch Plasma Screen TV", 5, "electronics", 1),
+    ("girlfriend", 6900.69, "other", 0),
+    ("John Nguyen", 0.01, "other", 69),
+    ("Feel_Special.mp3", 1.00, "music", 100),
+    ("shiba inu", 2500.95, "pets", 25),
+    ("jelly", 25.00, "food", 5);
